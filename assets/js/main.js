e3fea02a8e4d0e4c2be21da2c09a1ca00a7fb434
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-09-13 12:59:32 +0530

    updated code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 let currentYear = new Date().getFullYear();
 document.getElementById("current-year").textContent = currentYear;
+
+function formatToDisplay(date) {
+  const parsedDate = new Date(date);
+  return parsedDate.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+}
